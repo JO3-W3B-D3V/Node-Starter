446f0e4252d8c1d94e4f84a2e2c32537980ee4aa
JO3-W3B-D3V <joeevs196@gmail.com>
2020-08-23 21:33:36 +0100

    More House-Keeping
    Removed more bloat from the user controller.

--- src/user/UserController.js
+++ src/user/UserController.js
@@ -11,11 +11,10 @@ class UserController extends AbstractController {
   }
 
   async page(request, response, next) {
-    let data = null
-
     try {
       let page = 1
       let numberOfPages = 0
+      let users = null
 
       if (!isNull(request.query.page)) {
         page = request.query.page
@@ -28,88 +27,71 @@ class UserController extends AbstractController {
       if (page > numberOfPages) {
         this.notFound(page, `No users found on the page ${page}`)
       } else {
-        data = {
+        users = {
           results: await this.service.getUsersByPage(page),
           pages: numberOfPages,
         }
       }
+
+      response.setHeader('Content-Type', 'application/json')
+      response.status(200)
+      response.send(users)
     } catch (exception) {
       return next(createError(exception.status))
     }
-
-    response.setHeader('Content-Type', 'application/json')
-    response.status(200)
-    response.send(data)
   }
 
   async create(request, response, next) {
     try {
       const requestBody = request.body
       const contentType = request.headers['content-type']
-
       this.verifyJsonRequest(contentType)
       await this.service.createUser(requestBody)
+
+      response.setHeader('Content-Type', 'text/plain')
+      response.status(201)
+      response.send()
     } catch (exception) {
       return next(createError(exception.status))
     }
-
-    response.setHeader('Content-Type', 'text/plain')
-    response.status(201)
-    response.send()
   }
 
   async read(request, response, next) {
-    let data = null
-
     try {
       const id = request.params.id
-      data = await this.service.getUserById(id)
+      const user = await this.service.getUserById(id)
 
-      if (isNull(data)) {
+      if (isNull(user)) {
         this.notFound(id)
       }
+
+      response.setHeader('Content-Type', 'application/json')
+      response.status(200)
+      response.send(user)
     } catch (exception) {
       return next(createError(exception.status))
     }
-
-    response.setHeader('Content-Type', 'application/json')
-    response.status(200)
-    response.send(data)
   }
 
   async update(request, response, next) {
-    let data = null
-
     try {
       const contentType = request.headers['content-type']
-      const getId = () => {
-        if (!isNull(request.params.id)) {
-          return request.params.id
-        } else if (!isNull(request.body.id)) {
-          return request.body.id
-        } else {
-          return -1
-        }
-      }
-
-      const id = getId()
+      const getId = (r) => (!isNull(r.params.id) ? r.params.id : !isNull(r.body.id) ? r.body.id : -1)
+      const id = getId(request)
       this.verifyJsonRequest(contentType)
-      const user = await this.service.getUserById(id)
 
-      if (!isNull(user)) {
+      if (!isNull(await this.service.getUserById(id))) {
         request.body.id = id // Just in the event it was /users/:id
         await this.service.updateUser(request.body)
-        data = { forename: request.body.forename, surname: request.body.surname }
+        response.setHeader('Content-Type', 'application/json')
+        response.status(200)
+        response.send({ forename: request.body.forename, surname: request.body.surname })
       } else {
         this.notFound(id)
       }
     } catch (exception) {
       return next(createError(exception.status))
     }
-
-    response.setHeader('Content-Type', 'application/json')
-    response.status(200)
-    response.send(data)
   }
 
   async delete(request, response, next) {
